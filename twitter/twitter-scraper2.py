feb4a96733f14e7d5f94f9d169c6db2b48ca09b8
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-07-14 13:41:43 +0530

    completed twitter scrapping based on username

--- twitter/twitter-scraper2.py
+++ twitter/twitter-scraper2.py
@@ -3,15 +3,16 @@ from pprint import pprint
 import ast
 import csv
 
-LOAD_DATA = False
+LOAD_DATA = True
 # since -> start date
 # until -> last date
 
 if LOAD_DATA:
     scraper = Nitter(log_level=1, skip_instance_check=False)
-    tweets = scraper.get_tweets("akshitkhatri20", mode="user", number=5)
+    tweets = scraper.get_tweets("DemonSlayerSc", mode="user", number=5)
     pprint(tweets)
-    with open("data/tweets.txt", 'w', encoding='utf-8') as tweet:
+    file_name = "DemonSlayerSc"
+    with open(f"data/{file_name}_tweets.txt", 'w', encoding='utf-8') as tweet:
         tweet.write(str(tweets))
     LOAD_DATA = False
 else:
@@ -21,6 +22,7 @@ else:
 
 #pprint(tweets)
 tweets_csv = []
+user = ''
 for tweet in tweets['tweets']:
     tweet_csv = {
         'link': [],
@@ -37,10 +39,11 @@ for tweet in tweets['tweets']:
     tweet_csv['retweets'].append(tweet['stats']['retweets'])
     tweet_csv['comments'].append(tweet['stats']['comments'])
     tweets_csv.append(tweet_csv)
+    user = tweet['user']['name']
 
 pprint(tweets_csv)
 
-with open("data/tweets.csv", 'w', encoding='utf-8') as file:
+with open(f"data/{user}_tweets.csv", 'w', encoding='utf-8') as file:
     writer = csv.DictWriter(file, fieldnames=('link', 'text', 'user', 'quotes', 'retweets', 'comments'))
     writer.writeheader()
     writer.writerows(tweets_csv)
